4c3bb58e7628d10ff749276abbc4bbcca3cc4acb
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-29 17:17:15 -0300

    conceitos | valores padrão de função

--- parte_01/bundle.js
+++ parte_01/bundle.js
@@ -1,77 +1,25 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 6];
-/* 
+// valores padrão de função
+function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+}
 
-// função anônima
+; // a = 1, b = 6
 
-const newArr = arr.map(function(item) {
-  return item * 2;
+console.log(soma(1)); // a = 3, b = 6
 
-});
+console.log(soma()); // valores padrão em arrow function
 
-// arrow function - ideal para diminuir verbosidade do código
+var produto = function produto() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a * b;
+}; // a = 1, b = 6
 
-const newArr = arr.map((item) => {
-  return item * 2;
-});
 
-// arrow function - simplificação 1 parâmetro
+console.log(produto(1)); // a = 3, b = 6
 
-const newArr = arr.map(item => {
-  return item * 2;
-});
-
-*/
-// arrow function - simplificação função com processamento único
-
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr); // criação de função com constantes, não recomendável, dê preferência em utilizar function
-
-var teste1 = function teste1() {
-  return 'text';
-};
-
-console.log(teste1());
-
-var teste2 = function teste2() {
-  return [1, 2, 3];
-};
-
-console.log(teste2());
-
-var teste3 = function teste3() {
-  return 'teste';
-};
-
-console.log(teste3());
-
-var teste4 = function teste4() {
-  return 42;
-};
-
-console.log(teste4()); // retorna 'undefined', chaves também utilizada para corpo da função  
-
-var teste5a = function teste5a() {
-  nome: 'Mariano';
-};
-
-console.log(teste5a()); // para retornar objeto diretamente na função, é necessário incluí-lo entre parênteses
-
-var teste5b = function teste5b() {
-  return {
-    nome: 'Felipe'
-  };
-};
-
-console.log(teste5b()); // com a keyword return, parênteses não é necessário
-
-var teste5c = function teste5c() {
-  return {
-    nome: 'João'
-  };
-};
-
-console.log(teste5c());
+console.log(produto());
--- parte_01/main.js
+++ parte_01/main.js
@@ -1,63 +1,20 @@
-const arr = [1, 3, 4, 5, 6];
-
-/* 
-
-// função anônima
-
-const newArr = arr.map(function(item) {
-  return item * 2;
-
-});
-
-// arrow function - ideal para diminuir verbosidade do código
-
-const newArr = arr.map((item) => {
-  return item * 2;
-});
-
-// arrow function - simplificação 1 parâmetro
-
-const newArr = arr.map(item => {
-  return item * 2;
-});
-
-*/
-
-// arrow function - simplificação função com processamento único
-const newArr = arr.map(item => item * 2);
-
-console.log(newArr);
-
-
-// criação de função com constantes, não recomendável, dê preferência em utilizar function
-const teste1 = () => {
-  return 'text';
-}
-console.log(teste1());
-
-const teste2 = () => [1, 2, 3];
-console.log(teste2());
-
-const teste3 = () => 'teste';
-console.log(teste3());
-
-const teste4 = () => 42;
-console.log(teste4());
-
-// retorna 'undefined', chaves também utilizada para corpo da função  
-const teste5a = () => {
-  nome: 'Mariano'
+// valores padrão de função
+function soma(a = 3, b = 6) {
+  return a + b;
 };
-console.log(teste5a());
 
-// para retornar objeto diretamente na função, é necessário incluí-lo entre parênteses
-const teste5b = () => ({
-  nome: 'Felipe'
-});
-console.log(teste5b());
+// a = 1, b = 6
+console.log(soma(1));
 
-// com a keyword return, parênteses não é necessário
-const teste5c = () => {
-  return { nome: 'João'}
-};
-console.log(teste5c());
+// a = 3, b = 6
+console.log(soma());
+
+
+// valores padrão em arrow function
+const produto = (a = 3, b = 6) => a * b;
+
+// a = 1, b = 6
+console.log(produto(1));
+
+// a = 3, b = 6
+console.log(produto());
