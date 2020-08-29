db407f1bd694404ca1a3e1d331c7a27101d92965
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-28 23:48:50 -0300

    conceitos | const & let

--- parte_01/main.js
+++ parte_01/main.js
@@ -1,7 +1,26 @@
-class Matematica {
-  static soma(a, b) {
-    return a + b;
+// const não pode ter seu valor reatribuído
+const a = 1;
+a = 3;
+
+/* mutação é possível dentro de constante, quando objeto é trabalhado 
+com mesmo formato, alterando valores dentro de objeto ou vetor */
+const usuario = { nome: 'João'};
+usuario.nome = 'Cleiton';
+console.log(usuario);
+
+//let - keyword para variáveis de escopo
+function teste(x) {
+  let y = 2;
+
+  if (x>5) {
+    //reatribuição de valor permitido, dentro do escopo
+    let y = 4;
+
+    console.log(x, y);
   }
 }
 
-console.log(Matematica.soma(1,2))
+// erro, "y" não está definido, está fora do escopo
+console.log(y);
+
+teste(10);
