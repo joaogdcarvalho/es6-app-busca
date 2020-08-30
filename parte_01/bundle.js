68a61a868a4f307d9f4356e9b50eeca32dbb17bf
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-30 19:37:32 -0300

    conceitos | rest e spread

--- parte_01/bundle.js
+++ parte_01/bundle.js
@@ -1,28 +1,72 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+// OPERADOR REST - EXEMPLO 1
 var usuario = {
-  nome: 'Antonio',
-  idade: 29,
-  endereco: {
-    cidade: 'São Paulo',
-    estado: 'SP'
-  }
-}; // busca de valores 
-//const nome = usuario.nome;
-//const idade = usuario.idade;
-//const cidade = usuario.endereco.cidade;
-// busca de valores com conceito de desestruturação
+  nome: "Daniel",
+  idade: 40,
+  empresa: 'Rocketseat'
+};
 
 var nome = usuario.nome,
-    idade = usuario.idade,
-    cidade = usuario.endereco.cidade;
-console.log(nome);
-console.log(idade);
-console.log(cidade); // desestruturação com função - no caso está sendo passada propriedade específica de um objeto(usuário) para função
+    resto = _objectWithoutProperties(usuario, ["nome"]);
 
-function mostraNome(_ref) {
-  var nome = _ref.nome;
-  console.log(nome);
+console.log(nome);
+console.log(resto); // OPERADOR REST - EXEMPLO 2
+
+var arr = [1, 2, 3, 4, 5];
+var a = arr[0],
+    b = arr[1],
+    c = arr.slice(2);
+console.log(a);
+console.log(b);
+console.log(c); // OPERADOR REST - EXEMPLO 3
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 4)); // OPERADOR REST - EXEMPLO 4
+
+function restante(a, b) {
+  for (var _len2 = arguments.length, params = new Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
+    params[_key2 - 2] = arguments[_key2];
+  }
+
+  return params;
+}
+
+console.log(restante(1, 2, 3, 4, 5, 6, 7, 8, 9)); // SPREAD - EXEMPLO 1
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3); // SPREAD - EXEMPLO 2
+
+var usuario1 = {
+  nome: 'Felipe',
+  idade: 19,
+  empresa: 'Rocketseat'
+};
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario1), {}, {
+  nome: 'Gabriel'
+});
+
+console.log(usuario1);
+console.log(usuario2);
--- parte_01/main.js
+++ parte_01/main.js
@@ -1,27 +1,49 @@
-
+// OPERADOR REST - EXEMPLO 1
 const usuario = {
-  nome: 'Antonio',
-  idade: 29,
-  endereco: {
-    cidade: 'São Paulo',
-    estado: 'SP',
-  },
+  nome: "Daniel",
+  idade: 40,
+  empresa: 'Rocketseat',
+};
+const { nome, ...resto } = usuario;
+console.log(nome);
+console.log(resto);
+
+// OPERADOR REST - EXEMPLO 2
+const arr = [1, 2, 3, 4, 5];
+const [ a, b, ...c] = arr;
+console.log(a);
+console.log(b);
+console.log(c);
+
+// OPERADOR REST - EXEMPLO 3
+function soma(...params) {
+  return params.reduce((total, next) => total + next);
+}
+console.log(soma(1, 3, 4));
+
+// OPERADOR REST - EXEMPLO 4
+function restante(a, b, ...params) {
+  return params;
+}
+console.log(restante(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+// SPREAD - EXEMPLO 1
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+// SPREAD - EXEMPLO 2
+const usuario1 = {
+  nome: 'Felipe',
+  idade: 19,
+  empresa: 'Rocketseat',
 };
 
-// busca de valores 
-//const nome = usuario.nome;
-//const idade = usuario.idade;
-//const cidade = usuario.endereco.cidade;
+const usuario2 = {
+  ...usuario1,
+  nome: 'Gabriel'
+};
 
-// busca de valores com conceito de desestruturação
-const { nome, idade, endereco: { cidade } } = usuario;
-console.log(nome);
-console.log(idade);
-console.log(cidade);
-
-// desestruturação com função - no caso está sendo passada propriedade específica de um objeto(usuário) para função
-function mostraNome({ nome }) {
-  console.log(nome);
-}
-
-mostraNome(usuario);
+console.log(usuario1);
+console.log(usuario2);
