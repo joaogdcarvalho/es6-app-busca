d1dbc258d546691241c709df5cdcb0b97b556c81
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-08-30 20:23:45 -0300

    conceitos | template literals

--- parte_01/main.js
+++ parte_01/main.js
@@ -1,49 +1,8 @@
-// OPERADOR REST - EXEMPLO 1
-const usuario = {
-  nome: "Daniel",
-  idade: 40,
-  empresa: 'Rocketseat',
-};
-const { nome, ...resto } = usuario;
-console.log(nome);
-console.log(resto);
+const nome = 'Debora';
+const idade = 19;
 
-// OPERADOR REST - EXEMPLO 2
-const arr = [1, 2, 3, 4, 5];
-const [ a, b, ...c] = arr;
-console.log(a);
-console.log(b);
-console.log(c);
+// CONCATENAÇÃO DE STRINGS
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.');
 
-// OPERADOR REST - EXEMPLO 3
-function soma(...params) {
-  return params.reduce((total, next) => total + next);
-}
-console.log(soma(1, 3, 4));
-
-// OPERADOR REST - EXEMPLO 4
-function restante(a, b, ...params) {
-  return params;
-}
-console.log(restante(1, 2, 3, 4, 5, 6, 7, 8, 9));
-
-// SPREAD - EXEMPLO 1
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-const arr3 = [...arr1, ...arr2];
-console.log(arr3);
-
-// SPREAD - EXEMPLO 2
-const usuario1 = {
-  nome: 'Felipe',
-  idade: 19,
-  empresa: 'Rocketseat',
-};
-
-const usuario2 = {
-  ...usuario1,
-  nome: 'Gabriel'
-};
-
-console.log(usuario1);
-console.log(usuario2);
+// TEMPLATE LITERALS
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
