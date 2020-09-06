5e64fb1f8508cd48e7857d58e4567a28b685ff47
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-05 21:25:05 -0300

    conceitos | desafio 1, exercício 6

--- parte_01/desafio/main.js
+++ parte_01/desafio/main.js
@@ -1,38 +1,5 @@
-// 5.1 Rest
-const arr = [1, 2, 3, 4, 5, 6];
+// 6
+const usuario = 'Diego';
+const idade = 23;
 
-const[x, ...y] = arr;
-console.log(x);
-console.log(y);
-
-function soma(...args) {
-  return args.reduce((total, next) => total + next);
-};
-console.log(soma(1,2,10));
-
-// 5.2 Spread
-const usuario = {
-  nome: 'Diego',
-  idade: 23,
-  endereco: {
-    cidade: 'Rio do Sul',
-    uf: 'SC',
-    pais: 'Brasil',
-  }
-};
-
-const usuario2 = {
-  ...usuario, 
-  nome: 'Gabriel'
-};
-
-const usuario3 = {
-  ...usuario, 
-  endereco: {
-    ...usuario.endereco,
-    cidade: 'Lontras'
-  }
-};
-
-console.log(usuario2);
-console.log(usuario3);
+console.log(`O usuário ${usuario} possui ${idade} anos`);
