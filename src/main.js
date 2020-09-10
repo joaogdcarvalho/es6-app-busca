a78dd7f1a002e5733a706b894711c82d228e6f55
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-09 22:35:24 -0300

    webpack server | desafio 2, exercício 1 - ajuste import functions

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // 1
 import ClasseUsuario from "./functions";
 import { idade } from "./functions";
-import Usuario, { idade as IdadeUsuario } from '.functions';
+import Usuario, { idade as IdadeUsuario } from './functions';
 
 ClasseUsuario.info();
 
