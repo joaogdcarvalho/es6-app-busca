19b76fd1a862165723f3b12835fa65fd08046e66
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-12 16:20:54 -0300

    async/await | desafio 3, exercício 1

--- src/main.js
+++ src/main.js
@@ -1,17 +1,51 @@
 import axios from 'axios';
 
-class Api {
-  static async getUserInfo(username) {
-    try {
-      const response = await axios.get(`https://api.github.com/users/${username}`);
+// função delay aciona o .then após 1s
+const delay = (msgCont) => new Promise(
+  resolve => {
+      setTimeout(() => { resolve(`${msgCont} seg`) }, 1000);
+  }
+);
 
-      console.log(response);
+const umPorSegundo = async(sec) => {
+  let cont = 1;
+  while (cont <= sec) {
+    console.log(await delay(cont));
+    cont++;
+  }
+};
+
+class Github {
+  static async getUserFromGithub(user) {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${user}`);
+
+      console.log(response.data);
     }
     catch(err) {
-      console.warn('Erro na API');
+      console.warn('Usuário não existe');
     }
-  }
+  };
+
+  static async getRepositories(repo) {
+    try {
+      const response = await axios.get(`https://api.github.com/repos/${repo}`);
+
+      console.log(response.data);
+    }
+    catch(err) {
+      console.warn('Repositório não existe');
+    }
+  };
 }
 
-Api.getUserInfo('joaogdcarvalho');
-Api.getUserInfo('joaogdcarvalho8271hnb');
+umPorSegundo(3);
+
+Github.getUserFromGithub('joaogdcarvalho');
+Github.getUserFromGithub('joaogdcarvalh0');
+
+Github.getRepositories('rocketseat/awesome');
+Github.getRepositories('rocketseat/dslkvmskv');
+
+Github.getRepositories('joaogdcarvalho/joaogdcarvalho');
+Github.getRepositories('joaogdcarvalho');
