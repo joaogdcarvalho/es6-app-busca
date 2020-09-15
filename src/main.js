83b5c5a0b555e9c414db4c84ff9e114257ccfce4
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-14 23:13:11 -0300

    aplicação com ES6+ | render em tela

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
   }
@@ -21,7 +22,38 @@ class App {
       html_url: 'http://github.com/rocketseat/awesome',
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+
+    // map - utilizar quando há necessidade de percorrer e alterar o array de alguma forma.
+    //this.repositories.map();
+
+    // foreach - utilizar quando há necessidade apenas de percorrer o array.
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
